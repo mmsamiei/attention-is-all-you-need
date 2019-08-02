845c86701e76700ba4125e5c0d3df1591639e1d6
mmsamiei <mohmahsamiei@gmail.com>
2019-08-02 15:45:17 +0430

    add decoder_layer and decoder

---
+++ modules/decoder_layer.py
@@ -0,0 +1,38 @@
+import torch
+import torch.nn as nn
+import torch.optim as optim
+import torch.nn.functional as F
+
+import torchtext
+from torchtext.datasets import TranslationDataset, Multi30k
+from torchtext.data import Field, BucketIterator
+
+import spacy
+
+import random
+import math
+import os
+import time
+
+class DecoderLayer(nn.Module):
+    def __init__(self, hid_dim, n_heads, pf_dim, self_attention, positionwise_feedforward, dropout, device):
+        super().__init__()
+
+        self.ln = nn.LayerNorm(hid_dim)
+        self.sa = self_attention(hid_dim, n_heads, dropout, device)
+        self.ea = self_attention(hid_dim, n_heads, dropout, device)
+        self.pf = positionwise_feedforward(hid_dim, pf_dim, dropout)
+        self.do = nn.Dropout(dropout)
+
+    def forward(self, trg, src, trg_mask, src_mask):
+        #trg =  [batch_size, trg_Sent_len, hid_dim]
+        #src =  [batch_size, trg_Sent_len, hid_dim]
+        #trg_mask = [batch_size, trg_sent_len]
+        #src_mask = [batch_size, src_sent_len]
+
+        trg = self.ln(trg + self.do(self.sa(trg, trg, trg, trg_mask)))
+        trg = self.ln(trg + self.do(self.ea(trg, src, src, src_mask)))
+        trg = self.ln(trg + self.do(self.pf(trg)))
+
+        return trg
+
